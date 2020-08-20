d454ba7ffdde8931d38b682f26c5f93d41dd6640
nickup8 <nickup8@yandex.ru>
2020-08-20 19:39:04 +0300

    add img

--- src/pages/Home/Sections/SectionOne/SectionOne.jsx
+++ src/pages/Home/Sections/SectionOne/SectionOne.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import style from "./SectionOne.module.sass";
+import Header from "../../../../components/Header/Header";
 
 const SectionOne = () => {
-  return <div className={style.sectionOne}>Первая секция</div>;
+  return (
+    <div className={style.sectionOne}>
+      <div className="container">
+        <Header />
+      </div>
+    </div>
+  );
 };
 
 export default SectionOne;
