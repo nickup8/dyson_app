8fca03a1a2242846ec2187a37d06a1d9dcccc26a
nickup8 <nickup8@yandex.ru>
2020-08-20 23:44:34 +0300

    start redaction content from header

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import "./Home.module.sass";
 import SectionOne from "./Sections/SectionOne/SectionOne";
-import SectionTwo from "./Sections/SectionTwo/SectionTwo";
-import SectionThree from "./Sections/SectionThree/SectionThree";
-import SectionFour from "./Sections/SectionFour/SectionFour";
+// import SectionTwo from "./Sections/SectionTwo/SectionTwo";
+// import SectionThree from "./Sections/SectionThree/SectionThree";
+// import SectionFour from "./Sections/SectionFour/SectionFour";
 
 const Home = () => {
   return (
     <div className="home">
       <SectionOne />
-      <SectionTwo />
+      {/* <SectionTwo />
       <SectionThree />
-      <SectionFour />
+      <SectionFour /> */}
     </div>
   );
 };
--- src/pages/Home/Sections/SectionOne/Content/Content.jsx
+++ src/pages/Home/Sections/SectionOne/Content/Content.jsx
@@ -1,10 +1,41 @@
 import React from "react";
 import style from "./Content.module.sass";
+import { ReactComponent as ButtonIcon } from "./../../../../../images/iconButtonArrow.svg";
+import { ReactComponent as ButtonPlay } from "./../../../../../images/Play.svg";
 
 const Content = () => {
   return (
     <div className={style.content}>
-      <div className="left_column">Левая колонка</div>
+      <div className={style.left_column}>
+        <div className={style.text}>
+          <h1 className="h1">Увлажнитель воздуха Dyson AM10</h1>
+          <p className={style.description}>
+            С функцией вентилятора. Убивает до 99,9% бактерий в воде с помощью
+            безопасного УФ&nbsp;-&nbsp;излучения
+          </p>
+        </div>
+        <div className={style.buttonArea}>
+          <button className="button">
+            Оформить предзаказ
+            <span className="buttonIcon">
+              <ButtonIcon />
+            </span>
+          </button>
+        </div>
+        <div className="testimonials">
+          <div className="left">
+            <div className="name"></div>
+            <div className="rating"></div>
+            <div className="description"></div>
+          </div>
+          <div className="right">
+            <img src="src/images/imgVideo" alt="Play" />
+            <div className="buttonPlay">
+              <ButtonPlay />
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="center_column">Центр</div>
       <div className="right column">Правая колонка</div>
     </div>
