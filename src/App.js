d8e95b36d2efa8ffaba61494fed425706533684b
nickup8 <nickup8@yandex.ru>
2020-08-20 14:20:35 +0300

    Folder add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.sass';
+import Home from './pages/Home/Home';
 
 function App() {
 	return (
-		<div className="App">
-			Это App
-		</div>
+		<Home />
 	);
 }
 
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "Home.module.sass";
+
+const Home = () => {
+    return(
+        <div className="home">
+            Главная страница
+        </div>
+    );
+}
+
+export default Home;
